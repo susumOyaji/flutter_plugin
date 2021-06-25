39a3fad74d9b301f5e7bb823f2486bfbd75a4b99
susumOyaji <sumitomo0210@gmail.com>
2021-06-25 17:45:46 +0900

    up

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,56 +1,116 @@
-import 'package:flutter/material.dart';
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_plugin/flutter_plugin.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
+/*
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'URL Launcher',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'URL Launcher'),
+    );
+  }
+}
+*/
 
 class MyApp extends StatefulWidget {
+  //MyHomePage({Key key, this.title}) : super(key: key);
+  //final String title;
+
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  //Future<void> _launched;
+  String _phone = '09024922369';
 
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
+  // Get state.
+  String _androidphone = 'Dial';
+  String _hangstate = "HangUp";
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
+  //void setState() async {
+  //_androidphone = phonestate;
+  //_hangstate = hangupstate;
+  //}
+
+  Future<void> _getphonestate() async {
+    String phonestate;
+
     try {
-      platformVersion = await FlutterPlugin.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
+      final String result = await FlutterPlugin.phonetate;
+      phonestate = 'phone state at $result';
+    } on PlatformException catch (e) {
+      phonestate = "Failed to get androidphone: '${e.message}'.";
     }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
+    setState(() {
+      _androidphone = phonestate;
+      //_hangstate = hangupstate;
+    });
+  }
+
+  Future<void> _hangupstate() async {
+    String hangupstate;
+    try {
+      final String result = await FlutterPlugin.hangupstate;
+      hangupstate = 'phone state at $result';
+    } on PlatformException catch (e) {
+      hangupstate = "Failed to get androidphone: '${e.message}'.";
+    }
 
     setState(() {
-      _platformVersion = platformVersion;
+      _hangstate = hangupstate;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Plugin example app'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                  onChanged: (String text) => _phone = text,
+                  decoration: const InputDecoration(
+                      labelText: "incoming phonenumber",
+                      hintText: "{_androidphone}")),
+            ),
+            RaisedButton(
+              onPressed: () => setState(() {
+                _hangupstate();
+              }),
+              child: Text(_hangstate),
+            ),
+            RaisedButton(
+              onPressed: () => setState(() {
+                _getphonestate();
+              }),
+              child: Text(_androidphone),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+//flutter create --org com.sumitomo --template=plugin -i swift -a java ticketcall
--- lib/flutter_plugin.dart
+++ lib/flutter_plugin.dart
@@ -3,11 +3,25 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class FlutterPlugin {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_plugin');
+  static const MethodChannel _channel = const MethodChannel('flutter_plugin');
+  static const MethodChannel _platform =
+      const MethodChannel('samples.flutter.io/androidphone');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  static Future<String> get phonetate async {
+    String _phone;
+    final String phonestate =
+        await _platform.invokeMethod('androidphone', _phone);
+    return phonestate;
+  }
+
+  static Future<String> get hangupstate async {
+    //String hangupstate;
+    final String phonestate = await _platform.invokeMethod('hangup', true);
+    return phonestate;
   }
 }
